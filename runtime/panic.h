eec65f975c909a480fa75fe9bc47d058e385897c
Kevin He <32375681+kevin-he-01@users.noreply.github.com>
2022-06-22 12:05:51 -0700

    slight optimization on C side

--- runtime/panic.h
+++ runtime/panic.h
@@ -12,7 +12,7 @@ typedef enum {
 
 extern const char* panic_dumpfile;
 
-void panic(location_t location, reason_t reason, const char *fmt, ...);
+void panic(location_t location, reason_t reason, const char *fmt, ...) __attribute__ ((noreturn));
 void assert_type(location_t location, const char* operation, arg_t arg, type_t required_type);
 
 #endif /* COMPY_PANIC_H */
